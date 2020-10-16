7c531bbe1bd2b1c8be0676cfa4daeaf719791a0f
l-zvlv <l-zvlv@yandex.ru>
2020-10-16 22:12:57 +0400

    new

--- src/test/java/FirstTest/SelClass2.java
+++ src/test/java/FirstTest/SelClass2.java
@@ -2,10 +2,16 @@ package FirstTest;
 
 import PageObject.UnitPage;
 import SelenTest.SelClass;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.testng.annotations.Test;
 
 public class SelClass2 extends SelClass {
     @Test
+    @Epic("Селениум")
+    @Feature("Гугле тетс")
+    @Story("Проверяем картинки")
     public void metod () {
         getDriver().get("https://www.google.ru/");
         getDriver().manage().window().maximize();
